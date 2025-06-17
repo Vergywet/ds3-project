3477fb3c16072d07f8988e65cec433c9e203bcba
Vergywet <136698806+Vergywet@users.noreply.github.com>
2025-06-17 11:30:21 +0200

    Updated UI and removed trips page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -95,17 +95,14 @@ const routes: Routes = [
   {
     path: 'live-feed',
     loadChildren: () => import('./live-feed/live-feed.module').then( m => m.LiveFeedPageModule)
-  },  {
+  },
+  {
     path: 'profile',
     loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
   },
   {
     path: 'assigned-trip',
     loadChildren: () => import('./assigned-trip/assigned-trip.module').then( m => m.AssignedTripPageModule)
-  },
-  {
-    path: 'trips',
-    loadChildren: () => import('./trips/trips.module').then( m => m.TripsPageModule)
   },
 
   
